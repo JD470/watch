3f0f71439feb3206db486ae5a3c9b880e5c3dca1
JD470 <julien.dumesges@laposte.net>
2022-11-02 09:56:04 +0100

    Added key events

--- src/json_struct.rs
+++ src/json_struct.rs
@@ -1,15 +1,29 @@
 use serde_derive::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "snake_case")]
-pub struct ToWatch {
+pub struct to_watch {
     pub root: String,
     pub format: String,
     pub commands: Vec<String>,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 #[serde(rename_all = "snake_case")]
-pub struct Watch{
-    pub watch: ToWatch
+pub struct key_events{
+    pub events: Vec<key_event>
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub struct key_event{
+    pub keys: Vec<String>,
+    pub commands: Vec<String>
+}
+
+#[derive(Debug, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub struct watch{
+    pub watch: to_watch,
+    pub keys: key_events
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-use std::{process::Command, time::Duration};
+use std::{process::Command, time::Duration, str::FromStr};
 
 mod files;
 mod json_struct;
 
+use device_query::{DeviceState, DeviceQuery, Keycode};
 use files::*;
 use json_struct::*;
 
-fn main() {
+fn main(){
     let file = open_file("watch.json");
     
-    let to_watch: Watch = serde_json::from_str(file.as_str()).expect("JSON was not well-formatted");
+    let to_watch: watch = serde_json::from_str(file.as_str()).expect("JSON was not well-formatted");
     let root = to_watch.watch.root;
     let format = to_watch.watch.format;
     let commands = to_watch.watch.commands;
@@ -17,20 +18,41 @@ fn main() {
     let mut directory = get_all_files(&root, &format);
     let mut times: Vec<u64> = get_times(&directory);
 
+    let key_events = to_watch.keys.events;
+    
     loop{
         directory = get_all_files(&root, &format);
         let temp: Vec<u64> = get_times(&directory);
 
-        if compare_vecs(&times, &temp) {
+        if compare_times(&times, &temp) {
             times = temp;
             execute_args(&commands);
+        }
+
+        for i in &key_events{
+            let mut keys_pressed: Vec<Keycode> = get_keys_pressed();
+            if keys_pressed.len() != i.clone().keys.len(){
+                break;
+            }
+            let mut key_event: Vec<Keycode> = vec![];
+            for j in i.clone().keys{
+                key_event.push(Keycode::from_str(j.as_str()).unwrap());
+            }
+            keys_pressed = keys_pressed.into_iter().filter(|x| key_event.contains(x)).collect::<Vec<Keycode>>();
+            if keys_pressed.len() == key_event.len(){
+                execute_args(&i.clone().commands);
+            }
         }
 
         std::thread::sleep(Duration::from_millis(50));
     }
 }
 
-fn compare_vecs(temp1: &Vec<u64>, temp2: &Vec<u64>) -> bool{
+fn get_keys_pressed() -> Vec<Keycode>{
+    DeviceState::new().get_keys()
+}
+
+fn compare_times(temp1: &Vec<u64>, temp2: &Vec<u64>) -> bool{
     if temp1.len() != temp2.len(){
         return true;
     }
@@ -46,15 +68,16 @@ fn compare_vecs(temp1: &Vec<u64>, temp2: &Vec<u64>) -> bool{
 
 fn execute_args(commands: &Vec<String>){
     for i in commands{
-
         print!("{}",
             String::from_utf8
             (
-                Command::new("cmd").args
-                ([
+                Command::new("cmd")
+                .args(
+                [
                     "/C", &i.to_string()
-                ]).output().unwrap().stdout
+                ]
+                ).output().unwrap().stdout
             ).unwrap()
-        )
+        );
     }
 }
