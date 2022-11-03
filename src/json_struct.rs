b809a4df90fadbe6ddf0c89f8246a4597050a708
JD470 <julien.dumesges@laposte.net>
2022-11-03 14:03:50 +0100

    Optimizing dependencies(miniserde->nanoserde)

--- src/json_struct.rs
+++ src/json_struct.rs
@@ -1,28 +1,28 @@
-use miniserde::{Deserialize};
+use nanoserde::{DeJson};
 
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")]
+#[derive(Debug, DeJson, Clone)]
+#[nserde(rename_all = "snake_case")]
 pub struct ToWatch {
     pub root: String,
     pub format: String,
     pub commands: Vec<String>,
 }
 
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")]
+#[derive(Debug, DeJson, Clone)]
+#[nserde(rename_all = "snake_case")]
 pub struct KeyEvents{
     pub events: Vec<KeyEvent>
 }
 
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")]
+#[derive(Debug, DeJson, Clone)]
+#[nserde(rename_all = "snake_case")]
 pub struct KeyEvent{
     pub keys: Vec<String>,
     pub commands: Vec<String>
 }
 
-#[derive(Debug, Deserialize, Clone)]
-#[serde(rename_all = "snake_case")]
+#[derive(Debug, DeJson, Clone)]
+#[nserde(rename_all = "snake_case")]
 pub struct Watch{
     pub watch: ToWatch,
     pub keys: KeyEvents
