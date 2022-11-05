4abb7d3c8d0a57a66c7bbc02853604ceef6056c4
JD470 <julien.dumesges@laposte.net>
2022-11-05 18:36:31 +0100

    Shortened the code

--- src/run.rs
+++ src/run.rs
@@ -9,24 +9,23 @@ pub fn run(){
     let file = open_file("watch.json");
 
     let watch: Watch = nanoserde::DeJson::deserialize_json(file.as_str()).expect("JSON was not well-formatted");
-    let root = watch.to_watch.root;
-    let format = watch.to_watch.format;
-    let commands = watch.to_watch.commands;
+    let to_watch = watch.to_watch;
+    let keys = watch.keys;
     
-    let mut directory = get_all_files(&root, &format);
+    let mut directory = get_all_files(&to_watch.root, &to_watch.format);
     let mut times: Vec<u64> = get_times(&directory);
 
-    let key_events = watch.keys.events;
+    let key_events = keys.events;
     
     loop{
         // Watch
-        directory = get_all_files(&root, &format);
+        directory = get_all_files(&to_watch.root, &to_watch.format);
         let temp: Vec<u64> = get_times(&directory);
 
         if compare_times(&times, &temp) {
             let changed_file = get_changed_file_name(&directory, &times);
             times = temp;
-            execute_commands(&commands, &changed_file);
+            execute_commands(&to_watch.commands, &changed_file);
         }
 
         // Key events
@@ -35,6 +34,7 @@ pub fn run(){
             if keys_pressed.len() != i.keys.len(){
                 break;
             }
+
             let key_event: Vec<Keycode> = string_list_to_keycode_list(&i.keys);
             keys_pressed = get_matching_keycodes(keys_pressed, &key_event);
             
